f7c32103d0148900e9d992781c6291484dfc94b5
Yang <nicolas.yang.1@gmail.com>
2020-10-07 22:18:04 +0200

    Function add ok. Function checkUnckeck doesn't work

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # Todo List
 
 ## TODO
-    - Cliquer sur la ligne vide 
+
+    - Cliquer sur la ligne vide
     - Écrire une tache
     - Entrer ou cliquer sur + pour ajouter
+
 ### Ajouter
+
     - Écrire dans le input
     - Cliquer sur +
     - Ajouter le li avec class="todo" en début de liste
+
 ### Supprimer
     - Cliquer sur supprimer
+
 ### Modifier
-    - Cliquer sur le text
+
+    - Double Cliquer sur le text
     - Afficher le choix de modifier ou supprimer
+
 ### Check
+
     - Ajouter class="done" et faire descendre en avant dernier de la liste
--- js/app.js
+++ js/app.js
@@ -1,22 +1,88 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let add = document.querySelector('#add i')
-    let addTodo = document.querySelector('#add p')
-    let liste = document.querySelector('main section ul')
-    let deleteTodo, modifyTodo
+    let addIcon = document.querySelector('#add i')
+    let newTodoTitle = document.querySelector('#add input')
+    let todoListSection = document.querySelector('main section ul')
+    let todoInList = document.getElementsByTagName('li')
 
-    let todoList = ["Tache 1", "Tache 2", "Tache 3"]
+    let deleteTodo = document.getElementsByClassName('.fa-trsah')
+    let modifyTodo
 
-    add.addEventListener('click', () => {
-        todoList.push(addTodo.value)
-        console.log(todoList)
+
+    let todoArray = []
+
+    let Todo = function (title, status, position) {
+        this.title = title
+        this.status = status
+        this.position = position
+    }
+
+    //** TEST Status
+
+    let sportTodo = new Todo("sport", "todo")
+    let menageTodo = new Todo("ménage", "done")
+    let marcherTodo = new Todo("marcher", "todo")
+    let lingeTodo = new Todo("linge", "modif")
+
+    todoArray.push(sportTodo, menageTodo, marcherTodo, lingeTodo)
+    console.table(todoArray)
+
+
+
+    //** AFFICHER
+    let displayTodoList = () => {
+        todoListSection.innerHTML = ""
+        for (key in todoArray)
+            switch (todoArray[key].status) {
+                case ("todo"):
+                    todoListSection.innerHTML += `<li class="todo">${todoArray[key].title} <i class="far fa-square"></i></li>`
+                    break;
+                case ("done"):
+                    todoListSection.innerHTML += `<li class="done">${todoArray[key].title} <i class="far fa-check-square"></i></li>`
+                    break;
+                case ("modif"):
+                    todoListSection.innerHTML += `<li><input type="text" placeholder="${todoArray[key].title}"><i class="fas fa-edit"></i><i class="far fa-trash-alt">
+                </i></li>`
+                    break;
+                default:
+                    console.log("Default")
+            }
+    }
+    displayTodoList()
+
+    //** AJOUTER */
+    let addNewTodo = () => {
+        let newTodo = new Todo(`${newTodoTitle.value}`, "todo", todoArray[-1])
+        todoArray.push(newTodo)
+        console.table(todoArray)
+        displayTodoList()
+        newTodoTitle.value = ""
+    }
+
+    addIcon.addEventListener('click', () => {
+        newTodoTitle.value != "" ? addNewTodo() : false
     })
 
-    let displayTodoList = () => {
-        for (let todo in todoList) {
-            
+    //* CHECK UNCHECK
+    let checkUncheck = () => {
+        for (let i = 0; i < todoInList.length; i++) {
+            if (todoArray[i].status == "todo") {
+                todoArray[i].status = "done"
+            } else if (todoArray[i].status == "done") {
+                todoArray[i].status = "todo"
+            }
+            displayTodoList()
+            console.table(todoArray)
         }
     }
+
+    for (key in todoInList)
+        todoInList[key].addEventListener('click', () => {
+            console.log(todoInList[key])
+        })
+
+
+    //* REMOVE 
 
 
 })
